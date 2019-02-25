f2a9dce031ce91353e41c0a566a721777c758ed8
Peter Quinn <peter.quinn396@gmail.com>
2019-02-25 17:51:17 -0500

    added in RealNVP defintion

--- src/samplers/learned.h
+++ src/samplers/learned.h
@@ -40,20 +40,26 @@ class LearnedSampler : public Sampler {
 
 class RealNVP : public torch::nn::Module {
   public:
-    RealNVP(net_s, net_t, masks, prior);
-    torch::Tensor f(torch::Tensor x);
+    RealNVP(int num_dim);
+    torch::Tensor f(torch::Tensor x, torch::Tensor *jacobian);
     torch::Tensor g(torch::Tensor z);
-    float logProb(torch::Tensor x);
+    torch::Tensor logProb(torch::Tensor x);
+	
 
-  private:
+  private:    
+    std::vector<torch::nn::Sequential> s;
+    std::vector<torch::nn::Sequential> t;
+    std::vector<torch::Tensor> masks;
+    MultivariateNormal *prior;
+    int num_layers = 8;
+    int num_neurons = 40;
 };
 
 class MultivariateNormal {  // wrote this class in here b/c I am lazy and this
                             // isn't too big
   public:
     MultivariateNormal(int _dim) {
-        dim = _dim;
-        // should probably add some error checks on the matrix sizes
+        dim = _dim;      
     }
 
     torch::Tensor sample(int batchSize = 1) {
@@ -75,10 +81,10 @@ class MultivariateNormal {  // wrote this class in here b/c I am lazy and this
 
   private:
     int dim;
-    float stdDev = 1.f;
-    float mean = 0.f;
-    float SQRT_INV_2PI = 0.3989422804;
-    float LOG_SQRT_INV_2PI = -0.39908993417
+    float const stdDev = 1.f;
+    float const mean = 0.f;
+    float const SQRT_INV_2PI = 0.3989422804;
+    float const LOG_SQRT_INV_2PI = -0.39908993417;
 };
 
 Sampler *CreateLearnedSampler(const ParamSet &params);
